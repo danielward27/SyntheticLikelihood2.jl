bd2075af9c9b3936e6dced88266119b1e6f41b01
Documenter.jl <documenter@juliadocs.github.io>
2021-06-03 16:07:59 +0000

    build based on d33a3c0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SyntheticLikelihood2","category":"page"},{"location":"#SyntheticLikelihood2","page":"Home","title":"SyntheticLikelihood2","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SyntheticLikelihood2.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SyntheticLikelihood2]","category":"page"}]
+}
